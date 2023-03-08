4612a8189058a6322b487ebac0d0eb57263a339f
Pablo Embarach <pabloembarach@gmail.com>
2023-03-08 14:04:53 +0000

    Created SignInUp form module css and SignUpForm js. Also created Button module css on Styles.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1> } />
           <Route exact path="/signin" render={() => <h1>Sign in</h1> } />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1> } />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not Found!</p> } />
         </Switch>
       </Container>
