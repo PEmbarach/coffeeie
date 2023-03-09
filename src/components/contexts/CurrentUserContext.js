4781de8f5902f14f1ca5505ac620264935647ef9
Pablo Embarach <pabloembarach@gmail.com>
2023-03-09 17:44:05 +0000

    Import useMemo hook to cache response interceptor.

--- src/components/contexts/CurrentUserContext.js
+++ src/components/contexts/CurrentUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { axiosRes } from "../../api/axiosDefaults";
+import { response } from "msw";
 
 
 export const CurrentUserContext = createContext()
@@ -14,7 +16,7 @@ export const CurrentUserProvider = ({children}) => {
 
   const handleMount = async () => {
     try {
-      const {data} = await axios.get('dj-rest-auth/user/')
+      const {data} = await axiosRes.get('dj-rest-auth/user/')
       setCurrentUser(data)
     } catch(err){
       console.log(err)
@@ -24,6 +26,28 @@ export const CurrentUserProvider = ({children}) => {
   useEffect(() => {
     handleMount()
   }, []);
+  
+  useMemo(() => {
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401){
+          try{
+            await axios.post('dj-rest-auth/token/refesh/')
+          } catch (err){
+            setCurrentUser(prevCurrentUser => {
+              if (prevCurrentUser){
+                history.pushState('/signin')
+              }
+              return null
+            })
+          }
+          return axios(err.config)
+        }
+        return Promise.reject(err)
+      }
+    )
+  });
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
