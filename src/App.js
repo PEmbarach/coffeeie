60917bd98ca65e0612fd1e42c0110683e6fabb3c
Pablo Embarach <pabloembarach@gmail.com>
2023-03-09 14:53:18 +0000

    Crated and added SignIn event handler, onChange and submit handle, added errors alerts.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1> } />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1> } />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not Found!</p> } />
         </Switch>
