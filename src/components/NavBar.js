01d4908058d991280266a76fb22e13bf8897dd4a
Pablo Embarach <pabloembarach@gmail.com>
2023-03-10 11:11:47 +0000

    Created new variable addPostIcon and call it.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,21 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="fas fa-plus-square"></i>Add post
+    </NavLink>
+  )
+
   const loggedInIcons = <>{currentUser?.username}</>
   const loggedOutIcons =  (
     <>
@@ -35,6 +46,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
