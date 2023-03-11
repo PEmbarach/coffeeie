d8863960720e5ac6b829d5a7ecb3179c841b9a31
Pablo Embarach <pabloembarach@gmail.com>
2023-03-11 10:52:15 +0000

    Deleted a Col in SignIn/Up and center the form.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -40,7 +40,7 @@ const SignUpForm = () => {
   
   return (
     <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
+      <Col className="my-auto py-2 p-md-2" md={{ span: 5, offset: 4 }}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
@@ -119,18 +119,6 @@ const SignUpForm = () => {
           </Link>
         </Container>
       </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-          }
-        />
-      </Col>
-      
     </Row>
   );
 };
