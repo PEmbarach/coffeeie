9437804b3aa3ff911056861247a3e85818a9c722
Pablo Embarach <pabloembarach@gmail.com>
2023-03-14 13:58:11 +0000

    added the logic to store and update the state of the input field and update botton palette color.

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -17,7 +17,6 @@ import Asset from "../../components/Asset";
 import { Image } from "react-bootstrap";
 
 function PostCreateForm() {
-
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
@@ -118,14 +117,33 @@ function PostCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
+              { image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
                 <Form.Label
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
                   <Asset src={Upload} message="Click or tap to upload an image" />
                 </Form.Label>
-
+              )}
+                <Form.File
+                  id="image-upload" 
+                  accept="image/*" 
+                  onChange={handleChangeImage} 
+                />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>
