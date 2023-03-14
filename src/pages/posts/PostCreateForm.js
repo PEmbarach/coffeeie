e13b9bf6e934dc78423c22eb1e28309514da9f5c
Pablo Embarach <pabloembarach@gmail.com>
2023-03-14 14:25:07 +0000

    Imported useRef hook to declare a new imageInput variable, added the function to handle the form submission. Wire up the cancel button to take back to previous page.

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -15,6 +15,8 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 import { Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
@@ -28,6 +30,9 @@ function PostCreateForm() {
   });
 
   const { image, title, rate, price, location } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
     setPostData({
@@ -43,6 +48,27 @@ function PostCreateForm() {
         ...postData,
         image: URL.createObjectURL(event.target.files[0]),
       });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+
+    formData.append('image', imageInput.current.files[0])
+    formData.append('title', title)
+    formData.append('rate', rate)
+    formData.append('price', price)
+    formData.append('location', location)
+
+    try {
+      const {data} = await axiosReq.post('/posts/', formData);
+      history.push(`posts/${data.id}`)
+    } catch(err){
+      console.log(err)
+      if (err.response?.status !== 401){
+        setErrors(err.response?.data)
+      }
     }
   };
 
@@ -99,7 +125,7 @@ function PostCreateForm() {
         </InputGroup>
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -110,7 +136,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -140,9 +166,10 @@ function PostCreateForm() {
                 </Form.Label>
               )}
                 <Form.File
-                  id="image-upload" 
-                  accept="image/*" 
-                  onChange={handleChangeImage} 
+                  id="image-upload"
+                  accept="image/*"
+                  onChange={handleChangeImage}
+                  ref={imageInput}
                 />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
