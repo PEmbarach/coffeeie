424f1b60c48d36665ce7fa13d325f547e60fdb45
Pablo Embarach <pabloembarach@gmail.com>
2023-03-16 13:16:50 +0000

    Created Post.js and Post.module.css

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
 import styles from "./App.module.css";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { useEffect, useState } from "react";
-import { axiosReq } from "./api/axiosDefaults";
-
 
 function App() {
-  const { id } = useParams();
-  const [post, serPost] = useState({ results: [] });
-
-  useEffect (() => {
-    const handleMount = async () => {
-      try {
-        const [{data: post}] = await Promise.all([
-          axiosReq.get(`/posts/${id}`),
-        ])
-        setPost({results: [post]})
-        console.log(post)
-      } catch(err){
-        console.log(err)
-      }
-    }
-  })
-
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1> } />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-              <Route exact path="/posts/:id" render={()=> <PostPage />} />
-              <Route render={() => <p>Page not Found!</p> } />
-            </Switch>
-          </Container>
-        </div>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
-
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+  
+    useEffect (() => {
+      const handleMount = async () => {
+        try {
+          const [{data: post}] = await Promise.all([
+            axiosReq.get(`/posts/${id}`),
+            axiosReq.get(`/rate/${id}`),
+            axiosReq.get(`/details/${id}`),
+          ])
+          setPost({results: [post]});
+          console.log(post);
+        } catch(err){
+          console.log(err);
+        }
+      }
+  
+      handleMount();
+    }, [id]);
+  
 
   return (
     <Row className="h-100">
